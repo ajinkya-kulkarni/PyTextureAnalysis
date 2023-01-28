cea3e8cb673fcd1e9abaaa27059776fa5f8c35bd
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-01-28 18:27:52 +0100

    Fixed typos

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -81,15 +81,15 @@ with st.form(key = 'form1', clear_on_submit = False):
 	left_column1, middle_column1, right_column1  = st.columns(3)
 
 	with left_column1:
-		st.slider('Gaussian image filter sigma (pixels)', min_value = 0.1, max_value = 5.0, value = 1.0, step = 0.1, format = '%0.1f', label_visibility = "visible", key = '-FilterKey-')
+		st.slider('Gaussian image filter sigma [pixels]', min_value = 0.1, max_value = 5.0, value = 1.0, step = 0.1, format = '%0.1f', label_visibility = "visible", key = '-FilterKey-')
 		FilterKey = float(st.session_state['-FilterKey-'])
 
 	with middle_column1:
-		st.slider('Gaussian local window (pixels)', min_value = 2, max_value = 50, value = 10, step = 2, format = '%d', label_visibility = "visible", key = '-LocalSigmaKey-')
+		st.slider('Gaussian local window [pixels]', min_value = 2, max_value = 50, value = 10, step = 2, format = '%d', label_visibility = "visible", key = '-LocalSigmaKey-')
 		LocalSigmaKey = int(st.session_state['-LocalSigmaKey-'])
 
 	with right_column1:
-		st.slider('Threshold value for pixel evaluation (pixels)' , min_value = 5, max_value = 200, value = 20, step = 1, format = '%d', label_visibility = "visible", key = '-ThresholdValueKey-')
+		st.slider('Threshold value for pixel evaluation [pixels]' , min_value = 5, max_value = 200, value = 20, step = 1, format = '%d', label_visibility = "visible", key = '-ThresholdValueKey-')
 		ThresholdValueKey = int(st.session_state['-ThresholdValueKey-'])
 
 	####################################################################################
