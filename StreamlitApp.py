70a7ddeaacd4046c6185485b2d67c762bfeb40d6
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-01-28 18:26:47 +0100

    Added github repo link

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -66,6 +66,7 @@ DPI = 500
 # Title of the web app
 
 st.title(':blue[Texture analysis using PyTextureAnalysis]')
+st.caption('For more information, visit https://github.com/ajinkya-kulkarni/PyTextureAnalysis', unsafe_allow_html = False)
 
 st.markdown("")
 
