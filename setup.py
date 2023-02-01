f7aae1970dff43ee4e6f3a99e9246a42f1b7bafa
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-01 15:55:14 +0100

    Minor changes

--- setup.py
+++ setup.py
@@ -9,15 +9,13 @@ with open("README.md", "r") as fh:
 	setuptools.setup(
 	install_requires=['scipy','numpy','opencv-python-headless', 'scikit-image', 'matplotlib', 'Pillow'],
 	python_requires='>=3',
-	name='PyTextureAnalysis'
-	version=0.1.0,
+	name='PyTextureAnalysis',
+	version="0.1.0",
 	author="Ajinkya Kulkarni",
 	author_email="kulkajinkya@gmail.com",
 	description="PyTextureAnalysis is a Python package for analyzing the texture of images, specifically the local orientation, degree of coherence, and structure tensor of an image. This package is built using NumPy, SciPy and OpenCV.",
-	long_description = file: README.md, LICENSE.md
-	long_description_content_type = "text/markdown"
 	url="https://github.com/ajinkya-kulkarni/PyTextureAnalysis",
-	packages=setuptools.find_packages(),dirs for files to include
+	packages=setuptools.find_packages(),
 	include_package_data=True,
 	license_files=["LICENSE"],
 	classifiers=[
