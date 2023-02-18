090e8b409a671282782028fe5b19050a58abea34
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-18 11:16:29 +0100

    Minor UI changes

--- PyTextureAnalysis_StreamlitApp.py
+++ PyTextureAnalysis_StreamlitApp.py
@@ -348,7 +348,7 @@ with st.form(key = 'form1', clear_on_submit = False):
 			fig = plt.figure(figsize = FIGSIZE, constrained_layout = True, dpi = DPI)
 
 			#im = plt.imshow(Image_Coherance, vmin = 0, vmax = 1, cmap = 'Spectral_r')
-			im = plt.imshow(plt.cm.gray(binarized_image/binarized_image.max()) * plt.cm.Spectral_r(Image_Coherance), vmin = 0, vmax = 1, cmap = 'Spectral_r')
+			im = plt.imshow(plt.cm.binary_r(binarized_image/binarized_image.max()) * plt.cm.Spectral_r(Image_Coherance), vmin = 0, vmax = 1, cmap = 'Spectral_r')
 
 			plt.title('Coherence', pad = PAD, fontsize = FONTSIZE_TITLE)
 			plt.xticks([])
@@ -369,7 +369,7 @@ with st.form(key = 'form1', clear_on_submit = False):
 			fig = plt.figure(figsize = FIGSIZE, constrained_layout = True, dpi = DPI)
 
 			#im = plt.imshow(Image_Orientation/180, vmin = 0, vmax = 1, cmap = 'hsv')
-			im = plt.imshow(plt.cm.gray(binarized_image/binarized_image.max()) * plt.cm.hsv(Image_Orientation/180), vmin = 0, vmax = 1, cmap = 'hsv')
+			im = plt.imshow(plt.cm.binary_r(binarized_image/binarized_image.max()) * plt.cm.hsv(Image_Orientation/180), vmin = 0, vmax = 1, cmap = 'hsv')
 
 			plt.title('Orientation', pad = PAD, fontsize = FONTSIZE_TITLE)
 			plt.xticks([])
